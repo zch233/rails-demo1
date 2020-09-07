6861a88d5a3a5392238a4d816f64d566b593bb29
zhangchao <529743595@qq.com>
2020-09-07 19:39:42 +0800

    Add Rack_Lambda

---
+++ rack_lambda.rb
@@ -0,0 +1,9 @@
+require 'rack'
+require 'webrick'
+Rack::Handler::WEBrick.run ->(env) {
+  [
+    200,
+    {"Content-Type" => "text/html;charset=utf-8"},
+    ["<h1>rack_lambda</h1>", "<p>第二行内容</p>"]
+  ]
+},  Host: '0.0.0.0', Port: 1234
